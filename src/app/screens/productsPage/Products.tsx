cea621ada0181a49c1a2a27d8f95776d16639252
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-08-07 16:51:57 +0900

    fix: Refactor cart functionality: extract cart logic into useBasket hook, update components to use new hook, and pass cart handlers as props

--- src/app/screens/productsPage/Products.tsx
+++ src/app/screens/productsPage/Products.tsx
@@ -261,7 +261,6 @@ export default function Products(props: ProductsProps) {
                         <Button
                           className={"shop-btn"}
                           onClick={(e) => {
-                            console.log("Button Pressed!");
                             onAdd({
                               _id: product._id,
                               quantity: 1,
